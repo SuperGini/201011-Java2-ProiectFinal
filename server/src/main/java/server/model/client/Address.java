64b9bbf67839821758671255fe3a32b72ab252fc
Ginitoru <faraonu_ginitoru@yahoo.com>
2020-09-15 20:11:17 +0300

    made Adress class in ./server..../model/client package Adress => @Embeddable for Client class

---
+++ server/src/main/java/server/model/client/Address.java
@@ -0,0 +1,11 @@
+package server.model.client;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+
+    private String street;
+
+    private String number;
+}
---
+++ server/src/main/java/server/model/client/Client.java
@@ -0,0 +1,10 @@
+package server.model.client;
+
+public class Client {
+
+    private int id;
+
+    private String name;
+
+
+}
