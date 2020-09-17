3acdff63eb226a5967503381366b10e701d233dc
Ginitoru <faraonu_ginitoru@yahoo.com>
2020-09-18 00:39:54 +0300

    made VehicleConvertor class in server modulehas 2 method to convert Vehicle -> VehicleDto and VehicleDto -> vahicle

--- server/src/main/java/server/convert/autovehicle/VehicleConvetor.java
+++ server/src/main/java/server/convert/autovehicle/VehicleConvetor.java
@@ -1,9 +1,25 @@
 package server.convert.autovehicle;
 
+import lib.dto.autovehicle.VehicleDto;
+import server.model.autovehicle.Vehicle;
+
 public class VehicleConvetor {
 
     private VehicleConvetor() {
     }
 
-    
+    public static VehicleDto convert(Vehicle vehicle){
+        return new VehicleDto(
+                vehicle.getId(),
+                vehicle.getVehicleName(),
+                vehicle.getSerialNumber()
+        );
+    }
+
+    public static Vehicle convert(VehicleDto vehicleDto){
+        return new Vehicle(
+                vehicleDto.getVehicleName(),
+                vehicleDto.getSerialNumber()
+        );
+    }
 }
