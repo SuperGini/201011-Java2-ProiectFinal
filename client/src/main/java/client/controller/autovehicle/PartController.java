09f8f8db0b77d274d917f604e797415906fdd1a4
Ginitoru <faraonu_ginitoru@yahoo.com>
2020-09-19 00:43:53 +0300

    made PartController implements PartService in client module

---
+++ client/src/main/java/client/controller/autovehicle/PartController.java
@@ -0,0 +1,76 @@
+package client.controller.autovehicle;
+
+import lib.dto.autovehicle.PartDto;
+import lib.service.PartService;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Collection;
+
+public class PartController implements PartService {
+
+    private PartService partService;
+
+    public PartController() {
+        try {
+            Registry registry = LocateRegistry.getRegistry("localhost", 4545);
+            partService = (PartService) registry.lookup("partService");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public boolean createPart(PartDto partDto){
+        try {
+            return partService.createPart(partDto);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public PartDto findPartById(int id){
+        try {
+            return partService.findPartById(id);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public PartDto findPartByName(String name){
+        try {
+            return partService.findPartByName(name);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Collection<PartDto> findAllParts(){
+        try {
+            return partService.findAllParts();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final class SingletonHolder{
+        public final static VehicleController INSTANCE = new VehicleController();
+    }
+
+    public static VehicleController getInstance(){
+        return SingletonHolder.INSTANCE;
+    }
+}
