c2325b98afd62624fe9c86af264fc54905ea45fe
Ginitoru <faraonu_ginitoru@yahoo.com>
2020-09-24 21:31:50 +0300

    made ClientConvertor class

---
+++ server/src/main/java/server/convert/client/CleintConvertor.java
@@ -0,0 +1,30 @@
+package server.convert.client;
+
+import lib.dto.client.ClientDto;
+import server.model.client.Client;
+
+public class CleintConvertor {
+
+    private CleintConvertor() {
+    }
+
+
+    public static ClientDto convert(Client client){
+        ClientDto clientDto = new ClientDto();
+                    clientDto.setId(client.getId());
+                    clientDto.setName(client.getName());
+
+        return clientDto;
+        
+    }
+
+    public static Client convert(ClientDto clientDto){
+           Client client = new Client();
+                    client.setId(clientDto.getId());
+                    client.setName(clientDto.getName());
+
+           return client;
+
+    }
+
+}
