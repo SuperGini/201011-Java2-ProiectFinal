5ed6cb012b2b443d43baa007d21ee48c0c007cee
Ginitoru <faraonu_ginitoru@yahoo.com>
2020-10-04 03:02:15 +0300

    made MinimizeButton class and Graphics class -> they will be used to makethe minimize and close button

---
+++ client/src/main/java/client/util/Graphics.java
@@ -0,0 +1,21 @@
+package client.util;
+
+import java.awt.*;
+
+public class Graphics{
+
+    private Graphics() {
+    }
+
+    public static void graphicClose(Graphics2D graphics2D){
+
+
+
+    }
+
+    public static void graphicMinimize(Graphics2D graphics2D){
+        
+    }
+
+
+}
